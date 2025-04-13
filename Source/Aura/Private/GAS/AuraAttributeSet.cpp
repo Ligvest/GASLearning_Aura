b2eb16d49c728db3710ac655b3c35a4f5c7fcda4
Anatoly Dovzhik <anatoly.dovzhik@vokigames.com>
2025-04-13 10:51:27 +0200

    Fixed wrong playerState runtime error. Configured replication for attributes

--- Source/Aura/Private/GAS/AuraAttributeSet.cpp
+++ Source/Aura/Private/GAS/AuraAttributeSet.cpp
@@ -1,5 +1,48 @@
 // Dovzhik Tolya
 
-
 #include "GAS/AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	// You can Init props in ctor but can't use Setter here
+	InitHealth( 100.f );
+	InitMaxHealth( 100.f );
+	InitMana( 100.f );
+	InitMaxMana( 100.f );
+}
+
+// If in UPROPERTY we specify HOW the field is replicated in the function we specify WHEN the prop is replicated
+void UAuraAttributeSet::GetLifetimeReplicatedProps( TArray<class FLifetimeProperty>& OutLifetimeProps ) const
+{
+	Super::GetLifetimeReplicatedProps( OutLifetimeProps );
+
+	// This call should works for UPROEPRTY(Replicated) only. To replicate always without any callbacks ( e.g. no OnRep_Health )
+	// DOREPLIFETIME(UAuraAttributeSet, Health);
+	/* COND_None - Replicate always unconditionally */
+	/* REPNOTIFY_Always - each time call OnRep function ( OnRep_Health in this case ) */
+	DOREPLIFETIME_CONDITION_NOTIFY( UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always );
+	DOREPLIFETIME_CONDITION_NOTIFY( UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always );
+	DOREPLIFETIME_CONDITION_NOTIFY( UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always );
+	DOREPLIFETIME_CONDITION_NOTIFY( UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always );
+}
+void UAuraAttributeSet::OnRep_Health( const FGameplayAttributeData& OldHealth )
+{
+	// We can't just use Replicate as attributes replication in GAS should be forwarded to the GAS itself in the rep function
+	// So we forward the replication notification further to GAS so it could proccess it correctly
+	GAMEPLAYATTRIBUTE_REPNOTIFY( UAuraAttributeSet, Health, OldHealth );
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth( const FGameplayAttributeData& OldMaxHealth )
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY( UAuraAttributeSet, MaxHealth, OldMaxHealth );
+}
+void UAuraAttributeSet::OnRep_Mana( const FGameplayAttributeData& OldMana )
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY( UAuraAttributeSet, Mana, OldMana );
+}
+void UAuraAttributeSet::OnRep_MaxMana( const FGameplayAttributeData& OldMaxMana )
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY( UAuraAttributeSet, MaxMana, OldMaxMana );
+}
--- Source/Aura/Public/GAS/AuraAttributeSet.h
+++ Source/Aura/Public/GAS/AuraAttributeSet.h
@@ -3,15 +3,52 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "AuraAttributeSet.generated.h"
 
+// Accessors
+#define ATTRIBUTE_ACCESSORS( ClassName, PropertyName )           \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER( ClassName, PropertyName ) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER( PropertyName )               \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER( PropertyName )               \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER( PropertyName )
+
 /**
- * 
+ *
  */
 UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	UAuraAttributeSet();
+	// If in UPROPERTY we specify HOW the field is replicated in the function we specify WHEN the prop is replicated
+	virtual void GetLifetimeReplicatedProps( TArray<class FLifetimeProperty>& OutLifetimeProps ) const override;
+
+	// We can't just use Replicate becuase attributes replication in GAS should be forwarded to the GAS itself in the rep function
+	UPROPERTY( BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes" )
+	FGameplayAttributeData Health;
+	// Just to be able to get Health attribute or value, Set Base and current Health
+	ATTRIBUTE_ACCESSORS( UAuraAttributeSet, Health );
+	UPROPERTY( BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes" )
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS( UAuraAttributeSet, MaxHealth );
+	UPROPERTY( BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes" )
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS( UAuraAttributeSet, Mana );
+	UPROPERTY( BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes" )
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS( UAuraAttributeSet, MaxMana );
+
+	// When using ReplicatedUsing - after server changes a value it updates Health and calls this function
+	// So in OldHealth will be previous value of client's Health before replication
+	UFUNCTION()
+	void OnRep_Health( const FGameplayAttributeData& OldHealth );
+	UFUNCTION()
+	void OnRep_MaxHealth( const FGameplayAttributeData& OldMaxHealth );
+	UFUNCTION()
+	void OnRep_Mana( const FGameplayAttributeData& OldMana );
+	UFUNCTION()
+	void OnRep_MaxMana( const FGameplayAttributeData& OldMaxMana );
 };
